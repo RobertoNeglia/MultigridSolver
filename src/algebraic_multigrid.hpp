5756da3b44e679028196f7394f8c736193c98b9f
Roberto <roberto.neglia@mail.polimi.it>
2022-12-05 18:31:23 +0100

    algebraic_multigrid strength matrix construction

--- src/algebraic_multigrid.hpp
+++ src/algebraic_multigrid.hpp
@@ -1,3 +1,6 @@
+#ifndef __ALGEBRAIC_MULTIGRID_H_
+#define __ALGEBRAIC_MULTIGRID_H_
+
 #include <float.h>
 
 #include <algorithm>
@@ -7,35 +10,79 @@
 
 #include <Eigen/Eigen>
 
-using Matrix = Eigen::SparseMatrix<double>;
-
 // -5 -10 5 2 45 -23 0
 // 5 10 -5 -2 -45 23
 
 class AlgebraicMultigrid {
 private:
-  Matrix                       A;
+  Eigen::SparseMatrix<double>  A;
   std::map<int, std::set<int>> strength;
-  const double                 alpha = 1 / 4;
+  std::map<int, std::set<int>> strength_transpose;
+  std::map<int, int>           number_of_influences;
+  const double                 alpha = 1.0 / 4.0;
 
   void
-  construct_strength_set() {
+  construct_strength_sets() {
       for (int i = 0; i < A.rows(); i++) {
         double max = -DBL_MAX + 10;
+
           for (int k = 0; k < A.cols(); k++) {
             double a_ik = A.coeff(i, k);
-            if (-a_ik > max)
+            if (k != i && -a_ik > max)
               max = -a_ik;
           }
 
+        // std::cout << "Max of row " << i << " is: " << max << std::endl;
+
           for (int j = 0; j < A.cols(); j++) {
             double a_ij = A.coeff(i, j);
-            if (j != i && -a_ij >= alpha * max)
-              strength.at(i).insert(j);
+              if (j != i && -a_ij >= alpha * max) {
+                // std::cout << "a_ij: " << a_ij << std::endl;
+                strength[i].insert(j);
+            }
           }
+      }
+
+      for (auto [j, s_j] : strength) {
+          for (auto i : s_j) {
+            strength_transpose[i].insert(j);
+          }
+      }
+
+      for (auto [i, s_i] : strength_transpose) {
+        number_of_influences[i] = s_i.size();
+      }
+  }
+
+  void
+  print_strength_sets() {
+      for (auto [i, s_i] : strength) {
+        std::cout << "Points which influence point " << i << ":" << std::endl;
+
+          for (auto j : s_i) {
+            std::cout << "\t" << j << std::endl;
+          }
+      }
+
+      for (auto [i, s_i] : strength_transpose) {
+        std::cout << "Points that are influenced by point " << i << ":" << std::endl;
+
+          for (auto j : s_i) {
+            std::cout << "\t" << j << std::endl;
+          }
+      }
+
+      for (auto [i, lambda_i] : number_of_influences) {
+        std::cout << "Number of points influenced by " << i << ": " << lambda_i
+                  << std::endl;
       }
   }
 
 public:
-  AlgebraicMultigrid(Matrix A) : A(A) {}
+  AlgebraicMultigrid(Eigen::SparseMatrix<double> A) : A(A) {
+    construct_strength_sets();
+    print_strength_sets();
+  }
 };
+
+#endif
---
+++ src/test.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include "algebraic_multigrid.hpp"
+#include <Eigen/Eigen>
+
+/*
+    TODO:
+        - discretizzazione problema
+        - SERIAL
+            - implementazione matrice
+            - implementazione smoother (Jacobi/GS)
+            - coarsening
+
+        - PARALLEL
+            - parallelizzare smoother
+            - parallelizzare coarsening
+*/
+
+using Matrix = Eigen::SparseMatrix<double>;
+
+int
+main() {
+  int m = 3;
+  int n = 3;
+
+  Matrix P(m, n);
+  int    mn = m * n;
+  int    k  = 0;
+    for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+              P.coeffRef(i, j) = k;
+              k++;
+              std::cout << P.coeff(i, j) << " ";
+        }
+      std::cout << std::endl;
+    }
+  Matrix A(mn, mn);
+
+    for (int i = 0; i < mn; i++) {
+      A.coeffRef(i, i) = 4;
+      if (i > 0)
+        A.coeffRef(i, i - 1) = -1;
+      if (i > 2)
+        A.coeffRef(i, i - 3) = -1;
+      if (i < mn - 1)
+        A.coeffRef(i, i + 1) = -1;
+      if (i < mn - 3)
+        A.coeffRef(i, i + 3) = -1;
+    }
+
+  std::cout << A << std::endl;
+
+  AlgebraicMultigrid amg(A);
+  return 0;
+}
