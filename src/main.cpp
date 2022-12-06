724908b07db9f34085e38a093c95228f6e9aa095
pippooo <leonardobeltrame00@gmail.com>
2022-12-06 14:55:17 +0100

    Matrice CSR

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,13 @@ main() {
     Matrix m = Matrix(3,3);
     m.print();
     m.insertElement(3,0,1);
+    m.print();
     m.insertElement(-3,1,1);
+    m.insertElement(0,1,1);
+    m.insertElement(1,0,2);
+    //m.insert
+    m.insertElement(1,2,0);
+    m.insertElement(-1,2,2);
     m.print();
 }
 /*
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -18,53 +18,123 @@ public:
      
       for(unsigned int i = 0; i < nRow; i++)
          ia.emplace_back(-1);   // -1 indica riga di 0
-      ia.emplace_back(0);       //  l'ultimo elemento indica nnz
+      ia.emplace_back(0);      //  l'ultimo elemento indica nnz
+      
+      aa.reserve(nRow);
+      ja.reserve(nCol);    
   }
 
 
   void
   insertElement(double val, int i, int j)
-  {
-    if (val != 0) {
-      aa.emplace_back(val);
-      ja.emplace_back(j);
-      //int howmany = ia[i+1] - ia[i];
-      //for(int h = 0; h < howmany; h++)
-      //{
-        if (ia[i] == -1)
+  { 
+    if (val != 0.0) {
+      if (ia[i] != 0)
         {
+          //aa.emplace_back(val);
+          bool alias = false;
+          for (unsigned int l = 0; l < NNZRow(i); l++)
+          {
+            if (ja[ia[i] + l] == j) 
+            {
+                aa[ia[i] + l] = val;
+                alias = true;
+                break;
+            }    
+          }
+          if (!alias)
+          {
+          
+            double temp1, temp2;
+            int t1, t2;
+            temp1 = aa[i];
+            t1 = ja[i];
+            for (unsigned int n = ia[i] +1 ; n <= aa.size(); n++)
+            {   
+                temp2 = aa[n];    
+                aa[n] = temp1;
+                temp1 = temp2;          // inserimento nella posizione corretta
+                t2 = ja[n];
+                ja[n] = t1;
+                t1 = t2;
+            } 
+            aa[ia[i]] = val;  
+            ja[ia[i]] = j;
+        //na.emplace_back(n)
+        //int howmany = ia[i+1] - ia[i];
+        //for(int h = 0; h < howmany; h++)
+        //{
+        }
+        }
+        // else if (ia[i] == -1)
+        // {
+          aa.emplace_back(val);
+          ja.emplace_back(j);
+          if (i == nRow -1)
+            {
+              ia[i] = ia[i-1] +1;
+              ia[i+1]++;
+
+            }  
           for (unsigned int k = i+1; k < nRow; k++)
           {
-            if (ia[k] != -1)
+            if (ia[k] != -1 )
             {  
               ia[i] = ia[k];
-              for ( unsigned j = k; k < nRow; k++)
+              for ( unsigned n = k; k < nRow; k++)
               {
                 ia[k]++;
               }
               break;
-            }
-            else if (i != 0)
+            }  
+          }
+          if (i != 0)
+          {
               ia[i] = ia[i-1] + 1;
-            else
-              ia[i] = 0;  
+              ia[nRow]++;
           }
-          
-          for (unsigned int k = i+1; k < nRow + 1; k++)
-          {
-            if (ia[k] != -1)
-              ia[k] ++;
-          }
-        }
-        else
-        {
-          for (unsigned int k = i+1; k < nRow + 1; k++)
-          {
-            if (ia[k] != -1)
-              ia[k] ++;
-          }
-        }  
+          else
+              ia[i] = 0; 
+       // }
+        // else
+        // {
+        //   if (NNZRow(i) >= 1)
+        //   {
+        //     for (unsigned int k = i+1; k < nRow; k++)
+        //     {
+        //       if (ia[k] != -1)
+        //         ia[k] ++;
+        //     }
+        //     ia[nRow]++;
+        //   }
+        // }
+     }  
       //}
+     
+    // val = 0: sto inserendo elemento nullo corrisponde ad eliminare un elemento
+    else if(aa[i] != 0.0)      
+    {
+      bool alias = true;
+      for (unsigned int l = 0; l < NNZRow(i); l++)
+      {
+        if (ja[ia[i] + l] == j) 
+        {
+            aa[ia[i] + l] = val;
+            alias = false;
+            break;
+        }    
+      }
+      if (alias)
+      {
+        for (unsigned int n = ia[i] ; n < aa.size(); n++)
+        {
+            aa[n] = aa[n+1];
+            ja[n] = ja[n+1];
+        } 
+        //aa.erase(aa.size()-1);
+
+      }
+      
     }
 
   }
@@ -89,4 +159,26 @@ public:
     }
     cout << endl;
   }
+
+
+  int NNZRow(int index)
+  {
+    if (ia[index] == -1)
+      return 0;
+    for (unsigned int i = 1; i < ia.size() - index; i++)
+      if (ia[index + i] != -1)
+        return (ia[index+i] - ia[index]);
+    return 0;
+  }
+
+  // void sovrascrivi(double val1, double val2)
+  // {
+
+  // }
+
+  // bool binarySearch(double valore, vector<double> a, int inizio, int fine)
+  // {
+      
+  // }
 };
+
