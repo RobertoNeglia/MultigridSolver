ae52e838624b365f2112db7873e07cd4d9c04b4f
scorza9 <zaira.davide@gmail.com>
2022-12-06 17:02:24 +0100

    Add comments

--- src/matrixz.hpp
+++ src/matrixz.hpp
@@ -30,54 +30,58 @@ public:
   void
   insertElement(double val, int i, int j) {
       if (val != 0) {
-          if (A.size() == 0) {
+          if (A.size() == 0) { // empty matrix
             A.emplace_back(val);
             A_col.emplace_back(j);
             A_row[i] = 0;
             A_row[A_row.size() - 1]++;
 
-          } else {
-              if (A_row[i] == -1) {
+          } else { // not empty matrix
+
+            // A_row vector update
+              if (A_row[i] == -1) { // row i is empty
                 bool found = false;
-                  for (unsigned int k = i + 1; k < nRow; k++) {
+                  for (unsigned int k = i + 1; k < nRow; k++) { // find next row not empty
                       if (!found) {
                           if (A_row[k] != -1) {
                             A_row[i] = A_row[k];
                             found    = true;
                         }
-
-                      } else {
+                      } else { // I have found it!!! (update next rows indices)
                         if (A_row[k] != -1)
                           A_row[k]++;
                       }
                   }
-                  if (!found) {
+                  if (!found) { // update nnz
                     A_row[i] = A_row[A_row.size() - 1];
                 }
-            }
-
-              for (unsigned int k = i + 1; k < nRow + 1; k++) {
-                if (A_row[k] != -1)
-                  A_row[k]++;
-              }
-
-              if (getElement(i, j) != 0) {
-                getElementRef(i, j) = val;
               } else {
+                  for (unsigned int k = i + 1; k < nRow + 1; k++) {
+                    if (A_row[k] != -1)
+                      A_row[k]++;
+                  }
+              }
+              // update of A, A_col
+              if (getElement(i, j) !=
+                  0) { // if an element is already written in that position
+                getElementRef(i, j) = val;
+              } else { // zero in that position
                 bool found = false;
-                int  diff;
-                  for (int k = i + 1; k < nRow + 1 && !found; k++) {
-                       if (A_row[k] != -1) {
-                         diff  = A_row[k] - A_row[i];
-                         found = true;
+                int  diff;                                            // diff è una merda
+                  for (int k = i + 1; k < nRow + 1 && !found; k++) { //
+                      if (A_row[k] != -1) { // looks for the next non empty row
+                        diff =
+                          A_row[k] - A_row[i]; // diff is the number of elements on row i
+                        found = true;
                     }
                   }
-                  if (diff == 1) {
-                     A_col.insert(A_col.begin() + A_row[i], j);
-                     A.insert(A.begin() + A_row[i], val);
+                  if (diff == 1) { // only one element in that line
+                    A_col.insert(A_col.begin() + A_row[i], j); // updates A_col (?)
+                    A.insert(A.begin() + A_row[i], val);       // updates A (?)
 
-                  } else {
-                     bool found = false;
+                  } else { // if diff is greater than one (there is the problem that diff
+                           // is increased before, while updating A_row)
+                    bool found = false;
                       for (int k = 0; k < diff && !found; k++) {
                            if ((A_col[A_row[i] + k]) > j) {
                              A_col.insert(A_col.begin() + A_row[i] + k, j);
@@ -85,14 +89,14 @@ public:
                              found = true;
                         }
                       }
-                     if (!found)
+                    if (!found)
                       std::cout << "PIPPO" << endl;
                   }
               }
           }
     }
   }
-
+  // function getElementRef: if present, returns the reference to the element
   double &
   getElementRef(int i, int j) {
     bool found = false;
@@ -114,7 +118,7 @@ public:
           }
       }
   }
-
+  // If present, it returns the element in the given positio
   double
   getElement(int i, int j) {
       if (i < nRow && j < nCol) {
